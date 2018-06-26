6b21f07c13a7662b41055a50a953a0c78ae0f0aa
christoph <christoph@ThinkPad-E460>
2018-06-26 12:04:10 +0200

    shift Query_Execute Bug!

--- cae-project/Utilities/src/main/java/utilities/Query_Execute.java
+++ cae-project/Utilities/src/main/java/utilities/Query_Execute.java
@@ -1,10 +1,13 @@
-package org.UserInterface;
+package utilities;
 
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+
+import main.GUI;
+
 
 public class Query_Execute {
 

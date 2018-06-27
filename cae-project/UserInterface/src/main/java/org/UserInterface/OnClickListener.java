5e979aa07e2ac72a9fb29de3f93797fb3d092452
christoph <christoph@ThinkPad-E460>
2018-06-27 14:23:53 +0200

    small changes @ gui

--- cae-project/UserInterface/src/main/java/org/UserInterface/OnClickListener.java
+++ cae-project/UserInterface/src/main/java/org/UserInterface/OnClickListener.java
@@ -3,27 +3,28 @@ package org.UserInterface;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * implements click listener for all buttons at main pane
+ */
 public class OnClickListener implements ActionListener {
-    public void actionPerformed(ActionEvent ae) {
-	if (ae.getSource().equals("confBttn"))
-	    GUI.actConfig();
-	// TODO Vorsicht Chris! ich musste hier das getSource() für ein
-	// getActionCommand() ändern damit es funktioniert.
-	else if (ae.getActionCommand().equals("impBttn"))
-	    GUI.actImport();
-	else if (ae.getSource().equals("expBttn"))
-	    GUI.actExport();
-	else if (ae.getActionCommand().equals("addFilterBttn"))
-	    GUI.actAddFilter();
-	else if (ae.getActionCommand().equals("remoFilterBttn"))
-	    GUI.actRemoFilter();
-	else if (ae.getActionCommand().equals("searchBttn"))
-	    GUI.actSelect();
-	else if (ae.getActionCommand().equals("constructBttn"))
-	    GUI.actConstruct();
-	else if (ae.getActionCommand().equals("assumeBttn"))
-	    GUI.actAssume();
-	else if (ae.getActionCommand().equals("updateDbBttn"))
-	    GUI.actDbUpdate();
-    }
+	public void actionPerformed(ActionEvent ae) {
+		if (ae.getSource().equals("confBttn"))
+			GUI.actConfig();
+		else if (ae.getActionCommand().equals("impBttn"))
+			GUI.actImport();
+		else if (ae.getSource().equals("expBttn"))
+			GUI.actExport();
+		else if (ae.getActionCommand().equals("addFilterBttn"))
+			GUI.actAddFilter();
+		else if (ae.getActionCommand().equals("remoFilterBttn"))
+			GUI.actRemoFilter();
+		else if (ae.getActionCommand().equals("searchBttn"))
+			GUI.actSelect();
+		else if (ae.getActionCommand().equals("constructBttn"))
+			GUI.actConstruct();
+		else if (ae.getActionCommand().equals("assumeBttn"))
+			GUI.actAssume();
+		else if (ae.getActionCommand().equals("updateDbBttn"))
+			GUI.actDbUpdate();
+	}
 }
